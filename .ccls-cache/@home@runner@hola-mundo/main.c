9cee31dd3a0f968b6c8aaae02f5e83a8a11ec60b
4866958 <>
2022-08-25 17:40:30 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@hola-mundo/main.c
@@ -0,0 +1,15 @@
+/*Hola Mundo
+//Ivan Montaño
+//25-ago-2022
+//version 1*/
+
+//Bibliotecas
+
+
+//Funcion principal
+int main(void) {
+
+  //Funcion impresion en pantalla
+  printf("Hola Mundo\n");
+  return 0;
+}
